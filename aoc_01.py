6e7ebfa9987153267ea96c65040b7d26c823b06a
Jean Ramírez <jeanfed.ramirez@gmail.com>
2024-12-02 23:21:02 -0600

    Add final solution for day 1

--- aoc_01.py
+++ aoc_01.py
@@ -9,19 +9,26 @@ def main():
             right_list.append(int(line[1]))
             line = f.readline().split()
 
-    sum = 0 
+    # Create a read-only right list
+    right_list_ro = right_list.copy()
+
+    similarity_score = 0
+    total_distance = 0 
     while len(left_list) > 0: 
         # Find smallest from left_list and right_list
         left_min = min(left_list)
         right_min = min(right_list)
         # Compute the distance appart and add to the sum
         distance_appart = abs(left_min - right_min)
-        sum += distance_appart
+        total_distance += distance_appart
+        # Compute the similarity score
+        similarity_score += (right_list_ro.count(left_min) * left_min)
         # Remove the items from both lists
         left_list.remove(left_min)
         right_list.remove(right_min)
 
-    print(sum)
+    print("Total distance between lists: ", total_distance)
+    print("Similarity score between lists: ", similarity_score)
 
 
 if __name__ == "__main__":
